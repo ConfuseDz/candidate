08a75e17030f39859446742a383373525a6d78f5
chatchai.tangg <chatchai.tangg@gmail.com>
2022-09-12 23:18:22 +0700

    fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,41 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-const testUrl = 'https://jsonplaceholder.typicode.com/posts';
-function Home()  {
-  const [get, getPost] = useState(null);
+const testUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+const urlGetService = 'https://api-candidate.workforce-staging.com/v1/services';
+function Home() {
+  const [post, setPost] = useState(null);  
+    useEffect(()=>{
+      axios.get(urlGetService).then((res) => {
+        const myData = res.data;
+        setPost(myData);
+      })
+      .catch(error => console.error(`Error : ${error}`));
+    },[]);
 
-  useEffect(()=>{
-    axios.get(`${testUrl}/1`).then((res) => {
-      getPost(res.data);      
-    })
-  },[]);
-
-  function createPost(){
-    axios.post(testUrl, {
-      title: "Hello",
-      body: 'Testttt Post'
-    })
-    .then((resPost) => {
-      getPost(resPost.data);
-      console.log(resPost.data);
-    })
-  }
-
-  if (!get) return null;
+  
+  // function createPost(){
+  //   axios.post(testUrl, {
+  //     title: "Hello",
+  //     body: 'Testttt Post'
+  //   })
+  //   .then((resPost) => {
+  //     getPost(resPost.data);      
+  //   })
+  // }
+  
+  if (!post) return null;
 
     return (
       <div>
         <h1>Home</h1>
-        <h2>{get.title}</h2>
-        <p>{get.body}</p>
-        <button onClick={createPost}>Postt</button>
+        {post.map((p) =>{
+          return(
+            console.log(p.name)
+          )
+        })}
+        {/* <h2>{post.name}</h2> */}
+        {/* <button onClick={createPost}>Postt</button> */}
       </div>
     )
   };
